f6011db8f78f119d272d4054b5d83ea57f66e077
Rehan Sandhi <rsandhi37@gmail.com>
2026-02-23 06:47:44 +0530

    Remove documentation for getDisplayContent method
    Removed detailed documentation comment for getDisplayContent method.

--- app/src/main/java/acr/browser/lightning/browser/search/SearchBoxModel.kt
+++ app/src/main/java/acr/browser/lightning/browser/search/SearchBoxModel.kt
@@ -19,23 +19,9 @@ class SearchBoxModel @Inject constructor(
 
     private val untitledTitle: String = application.getString(R.string.untitled)
 
-    /**
-     * Returns the contents of the search box based on a variety of factors.
-     *
-     *  - The user's preference to show either the URL, domain, or page title
-     *  - Whether or not the current page is loading
-     *  - Whether or not the current page is a Lightning generated page.
-     *
-     * This method uses the URL, title, and loading information to determine what
-     * should be displayed by the search box.
-     *
-     * @param url       the URL of the current page.
-     * @param title     the title of the current page, if known.
-     * @param isLoading whether the page is currently loading or not.
-     * @return the string that should be displayed by the search box.
-     */
     fun getDisplayContent(url: String, title: String?, isLoading: Boolean): String =
         when {
+            url == "file:///android_asset/index.html" -> "RS Browser"
             url.isSpecialUrl() -> ""
             isLoading -> url
             else -> when (userPreferences.urlBoxContentChoice) {
